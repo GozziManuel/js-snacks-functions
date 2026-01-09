8168ba5f1e38de551c9bcfb1bb33889180fe95bb
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-01-09 15:54:41 +0100

    3s.

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,12 +2,20 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
-
+function detector(letter) {
+  const pushArray = [];
+  for (let i = 0; i < names.length; i++) {
+    const index = names[i];
+    const finder = index.indexOf(letter);
+    if (finder == 0) {
+      pushArray.push(index);
+    }
+  }
+  console.log(pushArray);
+  return pushArray;
+}
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+detector("A");
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
