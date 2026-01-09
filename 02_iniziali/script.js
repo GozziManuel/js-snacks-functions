91928d15ea5a7c8c61006d2ff7e7a4d3d823bdc5
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-01-09 15:22:36 +0100

    2s

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,17 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
-
+function firstLetter() {
+  const myLetters = [];
+  for (let i = 0; i < names.length; i++) {
+    const letter = names[i];
+    myLetters.push(letter[0]);
+  }
+  console.log(myLetters);
+  return myLetters;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+firstLetter();
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
