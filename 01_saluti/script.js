46c1d5cacb52ff419df94bfb4de31c86de7f05d4
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-01-09 14:35:52 +0100

    1s request

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,13 +1,12 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const userName = 'Mario';
-
+const userName = "Mario";
 
 // Dichiara la funzione qui.
-
+function myNameHello(name) {
+  console.log("ciao " + name);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+myNameHello(userName);
 //Risultato atteso se si passa 'Mario': // ciao Mario
