8bcd5d2ba68098b93c78f1d7fd3df49da44b197b
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-01-11 02:21:21 +0100

    bonus

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,14 +5,23 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
-
+const name = "Mario";
 
 // Dichiara la funzione qui.
-
-
+let timer = new Date();
+let hour = timer.getHours();
+console.log(hour);
+function clockTimer(username) {
+  if (hour >= 13 && hour <= 17) {
+    console.log("buon pomeriggio", username);
+  } else if (hour >= 18) {
+    console.log("buonsera", username);
+  } else if (hour >= 0 && hour < 6) {
+    console.log("buonsera", username);
+  } else if (hour >= 6) {
+    console.log("buongiorno", username);
+  }
+}
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+clockTimer(name);
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
